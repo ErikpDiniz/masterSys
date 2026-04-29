b67aa6ae021f7711b30ae529ab73d8d4c44bd705
Erik <erikdiniz2020@icloud.com>
2026-04-29 14:02:21 -0300

    Commit de integração.

---
+++ src/test/java/dev/erik/mastersys/MastersysApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.erik.mastersys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MastersysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
